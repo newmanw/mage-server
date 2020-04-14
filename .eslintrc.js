a450a6089558724b2101f12739b751498a65a662
restjohn <restjohn@users.noreply.github.com>
2020-04-14 11:56:33 -0600

    allow explicit any in eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,6 @@ module.exports =  {
       'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
       'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
       // 'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  
     ],
     parserOptions:  {
       ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
@@ -46,7 +45,8 @@ module.exports =  {
         // enable the rule specifically for TypeScript files
         "files": ["*.ts", "*.tsx"],
         "rules": {
-          "@typescript-eslint/explicit-function-return-type": ["error"]
+          "@typescript-eslint/explicit-function-return-type": ["error"],
+          "@typescript-eslint/no-explicit-any": "off",
         }
       },
       {
