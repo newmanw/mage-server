87ce33bbc7c07392d8347d0fd647b07ea2793cd3
restjohn <restjohn@users.noreply.github.com>
2020-04-21 10:56:54 -0600

    [service] refactoring manifold modules to follow plugins architecture

--- service/src/manifold/adapters/manifold.adapters.db.mongoose.ts
+++ service/src/manifold/adapters/manifold.adapters.db.mongoose.ts
@@ -1,33 +1,7 @@
 
 import mongoose, { Model, SchemaOptions } from 'mongoose'
 
-/**
- * An AdapterDescriptor represents a type of data source and the translation
- * from that data source type's data to data that MAGE can understand, and vice
- * versa.
- */
-export interface AdapterDescriptor {
-  id?: string
-  title: string
-  description: string
-  isReadable: boolean
-  isWritable: boolean
-  modulePath: string
-}
 
-/**
- * A SourceDescriptor represents an actual data endpoint whose data a
- * corresponding [[AdapterDescriptor|adapter]] can retrieve and transform.
- */
-export interface SourceDescriptor {
-  id?: string
-  adapter: string | AdapterDescriptor
-  title: string
-  description: string
-  isReadable: boolean
-  isWritable: boolean
-  url: string
-}
 
 export const ManifoldModels = {
   AdapterDescriptor: 'AdapterDescriptor',
