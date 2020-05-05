156ae35f2427927e99feab9cb5fec0ab673afdee
restjohn <restjohn@users.noreply.github.com>
2020-05-05 14:09:28 -0600

    [service] refactoring manifold modules into separate architectural layers

--- service/src/application/manifold/app.manifold.use_cases.ts
+++ service/src/application/manifold/app.manifold.use_cases.ts
@@ -1,5 +1,4 @@
-import { AdapterDescriptor, SourceDescriptor } from '../entities/manifold.entities';
-import { AdapterRepository, SourceRepository, ManifoldAuthorizationService } from './manifold.app.contracts';
+import { AdapterDescriptor, SourceDescriptor } from '../../entities/manifold/entities.manifold';
 import { MageError, MageErrorCode } from '../../application/app.global.errors';
 
 export interface ListAdaptersFn {
@@ -27,4 +26,30 @@ export function CreateSourceFn(adapterRepo: AdapterRepository, sourceRepo: Sourc
     }
     return await sourceRepo.create(sourceAttrs)
   }
+}
+
+export interface AdapterRepository {
+  readAll(): Promise<AdapterDescriptor[]>
+  /**
+   * Resolve null if no adapter descriptor with the given ID exists.
+   * @param adapterId
+   */
+  findById(adapterId: string): Promise<AdapterDescriptor | null>
+  /**
+   * Reject if there is no adapter descriptor with the given ID.
+   * @param attrs the attributes to update on the referenced adapter descriptor
+   */
+  update(attrs: Partial<AdapterDescriptor> & { id: string }): Promise<AdapterDescriptor>
+  removeById(adapterId: string): Promise<void>
+}
+
+export interface SourceRepository {
+  create(sourceAttrs: Partial<SourceDescriptor>): Promise<SourceDescriptor>
+  readAll(): Promise<SourceDescriptor[]>
+  findById(sourceId: string): Promise<SourceDescriptor | null>
+}
+
+export interface ManifoldAuthorizationService {
+  checkCurrentUserListAdapters(): Promise<void>
+  checkCurrentUserCreateSource(): Promise<void>
 }
