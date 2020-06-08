6360dc04c3562bfdf7f0b9cfd996ad835324ffb0
restjohn <restjohn@users.noreply.github.com>
2020-06-08 13:03:19 -0600

    [service] [feeds] add the concept of update frequency to topics and feeds

--- service/src/entities/feeds/entities.feeds.ts
+++ service/src/entities/feeds/entities.feeds.ts
@@ -82,6 +82,16 @@ export interface Feed {
   summary: string
   constantParams: Json
   variableParams: Json
+  /**
+   * A feed's update frequency is similar to the like-named property on its
+   * underlying topic.  While a topic's update frequency would come from the
+   * implementing plugin, a feed's update frequency would likely come from user
+   * configuration based on the parameters of the feed as well as the update
+   * frequency of the underlying topic.  This allows for feed service type
+   * plugins that are too generic to know what an appropriate update interval
+   * would be for particular service's topics.
+   */
+  updateFrequency: FeedUpdateFrequency | null
 }
 
 export type FeedParams = {
@@ -91,10 +101,19 @@ export type FeedParams = {
 
 export type FeedTopicId = string
 
+export class FeedUpdateFrequency {
+  constructor(readonly seconds: number) {}
+}
+
 export interface FeedTopic {
   readonly id: FeedTopicId
   readonly title: string
   readonly summary: string | null
   readonly constantParamsSchema: JSONSchema6 | null
   readonly variableParamsSchema: JSONSchema6 | null
+  /**
+   * A topic's update frequency is a hint about how often a service might
+   * publish new data to a topic.
+   */
+  readonly updateFrequency: FeedUpdateFrequency | null
 }
