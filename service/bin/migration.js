488a48faa306770d1455c5a76c470518f39a6dac
William Newman <newmanw10@gmail.com>
2020-10-08 13:19:34 -0600

    Update migration script

--- service/bin/migration.js
+++ service/bin/migration.js
@@ -1,19 +1,13 @@
 #! /usr/bin/env node
-
 /* eslint-disable @typescript-eslint/no-use-before-define */
-
 const path = require('path');
 const fs = require('fs');
 const { program: prog } = require('commander');
 const env = require('../lib/environment/env');
-const { Migrator } = require('mongodb-migrations');
-const { migrationCollection } = require('../lib/migrate');
-
-const migrationsDir = path.resolve(__dirname, '..', 'lib', 'migrations');
+const mongoose = require('mongoose');
+mongoose.set('debug', true)
 const srcMigrationsDir = path.resolve(__dirname, '..', 'src', 'migrations');
-
 prog.version('0');
-
 prog.command('create <name>')
   .description('generate a new migration script in the migrations directory')
   .action(name => {
@@ -31,56 +25,35 @@ prog.command('create <name>')
     fs.writeFileSync(migrationPath, stub);
     console.log(`created migration stub ${migrationPath}`);
   });
-
 prog.command('run')
   .description('apply all the migrations in lib/migrations directory.  make sure you npm run build first!')
-  .action(() => {
-    const migrator = new Migrator({
-      url: env.mongo.uri,
-      collection: migrationCollection,
-      options: env.mongo.options
-    })
-    migrator.runFromDir(migrationsDir,
-      (err, results) => {
-        setImmediate(() => {
-          console.log(JSON.stringify(results, null, 2));
-          if (err) {
-            console.log('migration failed: ', err);
-          }
-          // mongodb-migrations uses lodash.defer() to do the progress callbacks
-          // so they all run after the migration actually completes
-          migrator.dispose((err) => {
-            if (err) {
-              console.log('error shutting down: ', err);
-            }
-            else {
-              console.log('migration complete');
-            }
-          });
-        })
-      });
+  .action(async () => {
+    const { uri, connectRetryDelay, connectTimeout, options } = env.mongo
+    const { waitForDefaultMongooseConnection } = require('../lib/adapters/adapters.db.mongoose')
+    await waitForDefaultMongooseConnection(mongoose, uri, connectTimeout, connectRetryDelay, options)
+    const { runDatabaseMigrations } = require('../lib/migrate')
+    await runDatabaseMigrations(env.mongo.uri, env.mongo.options)
+    return mongoose.connection.close()
   });
-
 prog.parse(process.argv);
-
 function parseMigrationFileNames() {
   const files = fs.readdirSync(srcMigrationsDir);
-  return files.filter(function(f) {
+  return files.filter(function (f) {
     return path.extname(f) === '.js' && !f.startsWith('.');
-  }).map(function(f) {
+  }).map(function (f) {
     let ordinal = null;
     const numericPrefix = f.match(/^(\d+)/);
     if (numericPrefix) {
       ordinal = parseInt(numericPrefix[1]);
     }
     if (!ordinal) {
-      throw new Error(`migration file does not have numberic ordering prefix: ${f}`);
+      throw new Error(`migration file does not have numeric ordering prefix: ${f}`);
     }
     return {
       ordinal,
       name: f
     };
-  }).sort(function(f1, f2) {
+  }).sort(function (f1, f2) {
     return f1.ordinal - f2.ordinal;
   })
 }
