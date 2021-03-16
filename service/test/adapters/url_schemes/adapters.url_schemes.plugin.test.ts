a7662c2ac6a5e812b7cbc9616c566ee777c0f07e
restjohn <restjohn@users.noreply.github.com>
2021-03-15 20:15:26 -0600

    [service] another test for plugin url scheme

--- service/test/adapters/url_schemes/adapters.url_schemes.plugin.test.ts
+++ service/test/adapters/url_schemes/adapters.url_schemes.plugin.test.ts
@@ -31,4 +31,19 @@ describe.only('plugin url scheme', function() {
 
     expect(read).to.equal('The content you want')
   })
+
+  it('resolves content relative to dirname of the plugin index file', async function() {
+
+    const pluginName = '@adapters.url_schemes.plugin.test/plugin2'
+    const scheme = new PluginUrlScheme([ pluginName ], [ testNodeModules ])
+    const url = new URL(`mage-plugin:///${pluginName}/assets/some_content.txt`)
+    const content = await scheme.resolveContent(url) as NodeJS.ReadableStream
+    let read = await new Promise(function(this: { data: string[] }, resolve: (x: string) => any, reject: (x: any) => any) {
+      content.setEncoding('utf8')
+        .on('data', x => this.data.push(x))
+        .on('end', () => resolve(this.data.join()))
+    }.bind({ data: [] }))
+
+    expect(read).to.equal('I love you')
+  })
 })
