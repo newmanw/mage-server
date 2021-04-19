51ab40d67331f4dbd464048954f8e7ba16017241
restjohn <restjohn@users.noreply.github.com>
2021-04-19 16:07:05 -0600

    [web-app] [plugins] fix core lib public-api module

--- web-app/projects/core-lib/src/public-api.ts
+++ web-app/projects/core-lib/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of core-lib
  */
 
-export * from './lib/core-lib.service';
-export * from './lib/core-lib.component';
-export * from './lib/core-lib.module';
+export * from './lib/mage-core-lib.module'
