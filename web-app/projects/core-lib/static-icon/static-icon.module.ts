c925dad30f66a154c9a32c030708f390523774b5
restjohn <restjohn@users.noreply.github.com>
2021-04-23 10:38:07 -0600

    [web-app] [plugins] restore static-icon-select component because i know i'll forget about it and that will bite me later

--- web-app/projects/core-lib/static-icon/static-icon.module.ts
+++ web-app/projects/core-lib/static-icon/static-icon.module.ts
@@ -1,9 +1,11 @@
+import { ScrollingModule } from '@angular/cdk/scrolling'
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatCardModule } from '@angular/material/card'
 import { MageCommonModule } from '@ngageoint/mage.web-core-lib/common'
 import { StaticIconFormFieldComponent } from './static-icon-form-field/static-icon-form-field.component'
+import { StaticIconSelectComponent } from './static-icon-select/static-icon-select.component'
 
 
 @NgModule({
@@ -13,12 +15,15 @@ import { StaticIconFormFieldComponent } from './static-icon-form-field/static-ic
     ReactiveFormsModule,
     MatCardModule,
     MageCommonModule,
+    ScrollingModule
   ],
   declarations: [
     StaticIconFormFieldComponent,
+    StaticIconSelectComponent
   ],
   exports: [
-    StaticIconFormFieldComponent
+    StaticIconFormFieldComponent,
+    StaticIconSelectComponent
   ]
 })
 export class StaticIconModule {}
