f314a83ff4565ecb5554698a614c3025f30adb95
restjohn <restjohn@users.noreply.github.com>
2021-05-11 16:59:34 -0600

    [instance] fix resolution of web-app path for new project structure

--- instance/main.js
+++ instance/main.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 const { boot } = require('@ngageoint/mage.service/lib/app');
 
-const webappPackage = require.resolve('@ngageoint/mage.web-app/package');
-const webappDir = path.resolve(path.dirname(webappPackage), 'dist');
-const plugins = require('./plugins');
+const webappPackagePath = require.resolve('@ngageoint/mage.web-app/package.json');
+const webappPackage = require(webappPackagePath);
+const mainPath = webappPackage.main;
+const webappDir = path.resolve(path.dirname(webappPackagePath), mainPath);
+const config = require('./plugins.json');
 
 // TODO: collect environment variables
 
-boot({
-  plugins
-}).then(service => {
+boot(config).then(service => {
   service.app.use(express.static(webappDir));
   service.open();
 });
